bb7bc7ebd5d9d9707fe76c3145cdc3ab75e82fed
makomaiza3213 <makomaiza3213@gmail.com>
2021-03-25 17:28:22 -0300

    ejercicio resuelto

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,7 +1,11 @@
 texto = input('Ingrese un texto: ')
+
 palabra = input("Ingrese la palabra que desea buscar :")
+
 palabra = palabra.upper()
+
 texto2 = texto.upper()
+
 cant = texto2.count(palabra)
+
 print(cant)
-texto
